f7d93b50980893c4baf7319cce90bd16557b47a6
msohaaib <msohaib9685055@gmail.com>
2025-03-19 05:43:44 +0500

    updated

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth"; // ✅ Import Firebase Auth
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -12,6 +12,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app); // ✅ Initialize Auth
+const auth = getAuth(app);
 
-export { app, auth }; // ✅ Export 'auth'
+export { app, auth };
